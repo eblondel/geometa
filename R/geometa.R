d6d503f99bf05a5760a52e691b3ddbbf423763f4
eblondel <emmanuel.blondel1@gmail.com>
2017-04-07 10:50:11 +0200

    update ISO codelists

--- R/geometa.R
+++ R/geometa.R
@@ -11,7 +11,7 @@
 #'    Package: \tab geometa\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 0.1-0\cr
-#'    Date: \tab 2017-03-28\cr
+#'    Date: \tab 2017-04-07\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
