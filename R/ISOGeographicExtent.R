502901ea99990aa92ae25c4f70dec9333622219e
eblondel <emmanuel.blondel1@gmail.com>
2017-04-13 18:09:27 +0200

    add abstract class ISOGeographicExtent.R

---
+++ R/ISOGeographicExtent.R
@@ -0,0 +1,35 @@
+#' ISOGeographicExtent
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO extent
+#' @return Object of \code{\link{R6Class}} for modelling an ISO abstract geographicExtent
+#' @format \code{\link{R6Class}} object.
+#'
+#' @field extentTypeCode
+#'
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml, element, namespace, defaults)}}{
+#'    This method is used to instantiate an ISOGeographicExtent
+#'  }
+#' }
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISOGeographicExtent <- R6Class("ISOGeographicExtent",
+    inherit = ISOMetadataElement,
+    private = list(
+      xmlElement = "EX_GeographicExtent",
+      xmlNamespacePrefix = "GMD"
+    ),
+    public = list(
+      #+ extentTypeCode [0..1]: default "1"
+      extentTypeCode = NULL,
+      initialize = function(xml = NULL, element, namespace, defaults = list()){        
+        defaults = list(extentTypeCode = "1")
+        super$initialize(xml, element, namespace, defaults)
+      }
+    )                                          
+)
