056a77b2a1c233f36b98d9c9e4d8393c2da45b97
eblondel <emmanuel.blondel1@gmail.com>
2017-06-06 00:16:35 +0200

    #27 ISOUnlimitedInteger

---
+++ tests/testthat/test_ISOUnlimitedInteger.R
@@ -0,0 +1,25 @@
+# test_ISOUnlimitedInteger.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for ISOUnlimitedInteger.R
+#=======================
+require(geometa, quietly = TRUE)
+require(testthat)
+
+context("ISOUnlimitedInteger")
+
+test_that("encoding",{
+  
+  #encoding
+  md <- ISOUnlimitedInteger$new(value = 19L)
+  expect_is(md, "ISOUnlimitedInteger")
+  xml <- md$encode()
+  expect_is(xml, "XMLInternalNode")
+  
+  #decoding
+  md2 <- ISOUnlimitedInteger$new(xml = xml)
+  xml2 <- md2$encode()
+  
+  expect_true(ISOMetadataElement$compare(md, md2))
+  
+})
