6de94a1e49d5b349fc80f40b2b053ea421d5ef27
eblondel <emmanuel.blondel1@gmail.com>
2017-06-06 14:16:57 +0200

    fix examples

--- R/ISOFeatureAttribute.R
+++ R/ISOFeatureAttribute.R
@@ -46,17 +46,15 @@
 #'   uom$setUomSymbol("m")
 #'   md$setValueMeasurementUnit(uom)
 #'   
-#'   ISOListedValue( = "One")
-#'   
-#'   val1 <- ISOListedValue()
+#'   val1 <- ISOListedValue$new()
 #'   val1$setCode("code1")
 #'   val1$setLabel("label1")
-#'   val1$setDefinition("definition1)
+#'   val1$setDefinition("definition1")
 #'   md$addListedValue(val1)
-#'   val2 <- ISOListedValue()
+#'   val2 <- ISOListedValue$new()
 #'   val2$setCode("code2")
 #'   val2$setLabel("label2")
-#'   val2$setDefinition("definition12)
+#'   val2$setDefinition("definition2")
 #'   md$addListedValue(val2)
 #'   md$setValueType("typeName")
 #'  
