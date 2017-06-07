a7fa46794af00c8f6fa312678a8c356b17276d9f
eblondel <emmanuel.blondel1@gmail.com>
2017-06-08 00:07:22 +0200

    #44 support of RecordType

---
+++ R/ISORecordType.R
@@ -0,0 +1,43 @@
+#' ISORecordType
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO record
+#' @return Object of \code{\link{R6Class}} for modelling an ISORecordType
+#' @format \code{\link{R6Class}} object.
+#'
+#' @field value
+#'
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml,value)}}{
+#'    This method is used to instantiate an ISORecordType
+#'  }
+#' }
+#' 
+#' @references
+#'  ISO/TS 19103:2005 Geographic information -- Conceptual schema language
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISORecordType <- R6Class("ISORecordType",
+   inherit = ISOMetadataElement,
+   private = list(
+     xmlElement = "RecordType",
+     xmlNamespacePrefix = "GCO"
+   ),
+   public = list(
+     value = NA,
+     initialize = function(xml = NULL, value){
+       super$initialize(
+         xml = xml,
+         element = private$xmlElement,
+         namespace = getISOMetadataNamespace(private$xmlNamespacePrefix)
+       )
+       if(is.null(xml)){
+         self$value = value
+       }
+     }
+   )                        
+)
---
+++ tests/testthat/test_ISORecordType.R
@@ -0,0 +1,26 @@
+# test_ISORecordType.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for ISORecordType.R
+#=======================
+require(geometa, quietly = TRUE)
+require(testthat)
+
+context("ISORecordType")
+
+test_that("encoding",{
+  
+  #encoding
+  md <- ISORecordType$new(value = "myvalue")
+  expect_is(md, "ISORecordType")
+  expect_equal(md$value, "myvalue")
+  xml <- md$encode()
+  expect_is(xml, "XMLInternalNode")
+  
+  #decoding
+  md2 <- ISORecordType$new(xml = xml)
+  xml2 <- md2$encode()
+  
+  expect_true(ISOMetadataElement$compare(md, md2))
+  
+})
