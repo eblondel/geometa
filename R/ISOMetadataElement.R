100db8ce374e5d039a0e07b5581c766b2a7b8a4c
eblondel <emmanuel.blondel1@gmail.com>
2017-06-14 02:13:31 +0200

    more attempt to fix onLoad

--- R/ISOMetadataElement.R
+++ R/ISOMetadataElement.R
@@ -576,9 +576,8 @@ ISOMetadataElement$compare = function(metadataElement1, metadataElement2){
 fetchISOSchemas <- function(){
   packageStartupMessage("Loading ISO 19139 XML schemas... \n")
   xsdfile <- system.file("extdata/schemas/gmd", "gmd.xsd", package = "geometa", mustWork = TRUE)
-  loadNamespace("XML")
   .geometa.iso.schemas <- tryCatch(
-    xmlTreeParse(
+    XML::xmlParse(
       xsdfile, isSchema = TRUE, xinclude = TRUE,
       error = function (msg, code, domain, line, col, level, filename, class = "XMLError"){}
     )
