439b7de261c101e2ad55990631d0c64b959a85bf
eblondel <emmanuel.blondel1@gmail.com>
2018-09-21 22:51:27 +0200

    #115 GMLTimeInstant

---
+++ R/GMLTimeInstant.R
@@ -0,0 +1,56 @@
+#' GMLTimeInstant
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO time instant
+#' @return Object of \code{\link{R6Class}} for modelling an GMLTimeInstant
+#' @format \code{\link{R6Class}} object.
+#'
+#' @field timePosition
+#'
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml, timePosition)}}{
+#'    This method is used to instantiate an GMLTimeInstant
+#'  }
+#'  \item{\code{setTimePosition(timePosition)}}{
+#'    Sets the position (date or date and time of the resource contents), 
+#'    as object of class "POSIXct"/"POSIXt" or "Date"
+#'  }
+#' }
+#' 
+#' @examples 
+#'   time <- ISOdate(2000, 1, 12, 12, 59, 45)
+#'   md <- GMLTimeInstant$new(timePosition = time)
+#'   xml <- md$encode()
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+GMLTimeInstant <- R6Class("GMLTimeInstant",
+   inherit = GMLAbstractTimeGeometricPrimitive,
+   private = list(
+     xmlElement = "TimeInstant",
+     xmlNamespacePrefix = "GML"
+   ),
+   public = list(
+     #+ timePosition [1]: 'POSIXct','POSIXt'
+     timePosition = NULL,
+     initialize = function(xml = NULL, timePosition){
+       super$initialize(xml = xml)
+       if(is.null(xml)){
+         self$setTimePosition(timePosition)
+       }
+     },
+     
+     #setTimePosition
+     setTimePosition = function(timePosition){
+       if(!all(class(timePosition)==c("POSIXct","POSIXt")) | is(timePosition, "Date")){
+         stop("Value should be of class ('POSIXct','POSIXt') or 'Date'")
+       }
+       self$timePosition <- GMLElement$create("timePosition", value = timePosition)
+     }
+   )                        
+)
+
+
---
+++ tests/testthat/test_GMLTimeInstant.R
@@ -0,0 +1,27 @@
+# test_GMLTimeInstant.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for GMLTimeInstant.R
+#=======================
+require(geometa, quietly = TRUE)
+require(testthat)
+require(XML)
+
+context("GMLTimeInstant")
+
+test_that("encoding",{
+  
+  #encoding
+  time <- ISOdate(2000, 1, 12, 12, 59, 45)
+  md <- GMLTimeInstant$new(timePosition = time)
+  
+  xml <- md$encode()
+  expect_is(xml, "XMLInternalNode")
+  
+  #decoding
+  md2 <- GMLTimeInstant$new(xml = xml)
+  xml2 <- md2$encode()
+  
+  expect_true(ISOAbstractObject$compare(md, md2))
+  
+})
