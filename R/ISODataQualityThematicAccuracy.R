b2c8f2940d069a841d71ab2c703581f32dd87b4c
eblondel <emmanuel.blondel1@gmail.com>
2019-01-14 09:56:50 +0100

    #71 DQ ThematicAccuracy classes

---
+++ R/ISODataQualityThematicAccuracy.R
@@ -0,0 +1,214 @@
+#' ISOAbstractThematicAccuracy
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO data quality abstract thematic accuracy
+#' @return Object of \code{\link{R6Class}} for modelling an ISOAbstractThematicAccuracy
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Inherited methods:
+#' \describe{
+#'   from \code{ISODataQualityAbstractElement}
+#' }
+#' 
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml)}}{
+#'    This method is used to instantiate an ISOAbstractThematicAccuracy
+#'  }
+#' }
+#'   
+#' @references 
+#'   ISO 19115:2003 - Geographic information -- Metadata 
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISOAbstractThematicAccuracy <- R6Class("ISOAbstractThematicAccuracy",
+ inherit = ISODataQualityAbstractElement,
+ private = list(
+   xmlElement = "AbstractDQ_ThematicAccuracy",
+   xmlNamespacePrefix = "GMD"
+ ),
+ public = list()
+)
+
+#' ISOQuantitativeAttributeAccuracy
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO data quality quantitative attribute accuracy
+#' @return Object of \code{\link{R6Class}} for modelling an ISOQuantitativeAttributeAccuracy
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Inherited methods:
+#' \describe{
+#'   from \code{ISODataQualityAbstractElement}
+#' }
+#' 
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml)}}{
+#'    This method is used to instantiate an ISOQuantitativeAttributeAccuracy
+#'  }
+#' }
+#' 
+#' @examples 
+#'   #encoding
+#'   dq <- ISOQuantitativeAttributeAccuracy$new()
+#'   dq$addNameOfMeasure("measure")
+#'   metaId <- ISOMetaIdentifier$new(code = "measure-id")
+#'   dq$setMeasureIdentification(metaId)
+#'   dq$setMeasureDescription("description")
+#'   dq$setEvaluationMethodDescription("method description")
+#'   dq$setEvaluationMethodType("indirect")
+#'   dq$setDateTime(ISOdate(2015,1,1,12,10,49))
+#'   spec <- ISOCitation$new()
+#'   spec$setTitle("specification title")
+#'   spec$setAlternateTitle("specification alternate title")
+#'   d <- ISODate$new()
+#'   d$setDate(ISOdate(2015, 1, 1, 1))
+#'   d$setDateType("publication")
+#'   spec$addDate(d)
+#'   dq$setEvaluationProcedure(spec)
+#'   result <- ISOConformanceResult$new()
+#'   result$setSpecification(spec)
+#'   result$setExplanation("some explanation about the conformance")
+#'   result$setPass(TRUE)
+#'   dq$addResult(result)
+#'   xml <- dq$encode()
+#'   
+#' @references 
+#'   ISO 19115:2003 - Geographic information -- Metadata 
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISOQuantitativeAttributeAccuracy <- R6Class("ISOQuantitativeAttributeAccuracy",
+ inherit = ISOAbstractThematicAccuracy,
+ private = list(
+   xmlElement = "DQ_QuantitativeAttributeAccuracy",
+   xmlNamespacePrefix = "GMD"
+ ),
+ public = list()
+)
+
+#' ISONonQuantitativeAttributeAccuracy
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO data quality nonquantitative attribute accuracy
+#' @return Object of \code{\link{R6Class}} for modelling an ISONonQuantitativeAttributeAccuracy
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Inherited methods:
+#' \describe{
+#'   from \code{ISODataQualityAbstractElement}
+#' }
+#' 
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml)}}{
+#'    This method is used to instantiate an ISONonQuantitativeAttributeAccuracy
+#'  }
+#' }
+#' 
+#' @examples 
+#'   #encoding
+#'   dq <- ISONonQuantitativeAttributeAccuracy$new()
+#'   dq$addNameOfMeasure("measure")
+#'   metaId <- ISOMetaIdentifier$new(code = "measure-id")
+#'   dq$setMeasureIdentification(metaId)
+#'   dq$setMeasureDescription("description")
+#'   dq$setEvaluationMethodDescription("method description")
+#'   dq$setEvaluationMethodType("indirect")
+#'   dq$setDateTime(ISOdate(2015,1,1,12,10,49))
+#'   spec <- ISOCitation$new()
+#'   spec$setTitle("specification title")
+#'   spec$setAlternateTitle("specification alternate title")
+#'   d <- ISODate$new()
+#'   d$setDate(ISOdate(2015, 1, 1, 1))
+#'   d$setDateType("publication")
+#'   spec$addDate(d)
+#'   dq$setEvaluationProcedure(spec)
+#'   result <- ISOConformanceResult$new()
+#'   result$setSpecification(spec)
+#'   result$setExplanation("some explanation about the conformance")
+#'   result$setPass(TRUE)
+#'   dq$addResult(result)
+#'   xml <- dq$encode()
+#'   
+#' @references 
+#'   ISO 19115:2003 - Geographic information -- Metadata 
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISONonQuantitativeAttributeAccuracy <- R6Class("ISONonQuantitativeAttributeAccuracy",
+  inherit = ISOAbstractThematicAccuracy,
+  private = list(
+    xmlElement = "DQ_NonQuantitativeAttributeAccuracy",
+    xmlNamespacePrefix = "GMD"
+  ),
+  public = list()
+)
+
+#' ISOThematicClassificationCorrectness
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO data quality thematic accuracy classification correctness
+#' @return Object of \code{\link{R6Class}} for modelling an ISOThematicClassificationCorrectness
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Inherited methods:
+#' \describe{
+#'   from \code{ISODataQualityAbstractElement}
+#' }
+#' 
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml)}}{
+#'    This method is used to instantiate an ISOThematicClassificationCorrectness
+#'  }
+#' }
+#' 
+#' @examples 
+#'   #encoding
+#'   dq <- ISOThematicClassificationCorrectness$new()
+#'   dq$addNameOfMeasure("measure")
+#'   metaId <- ISOMetaIdentifier$new(code = "measure-id")
+#'   dq$setMeasureIdentification(metaId)
+#'   dq$setMeasureDescription("description")
+#'   dq$setEvaluationMethodDescription("method description")
+#'   dq$setEvaluationMethodType("indirect")
+#'   dq$setDateTime(ISOdate(2015,1,1,12,10,49))
+#'   spec <- ISOCitation$new()
+#'   spec$setTitle("specification title")
+#'   spec$setAlternateTitle("specification alternate title")
+#'   d <- ISODate$new()
+#'   d$setDate(ISOdate(2015, 1, 1, 1))
+#'   d$setDateType("publication")
+#'   spec$addDate(d)
+#'   dq$setEvaluationProcedure(spec)
+#'   result <- ISOConformanceResult$new()
+#'   result$setSpecification(spec)
+#'   result$setExplanation("some explanation about the conformance")
+#'   result$setPass(TRUE)
+#'   dq$addResult(result)
+#'   xml <- dq$encode()
+#'   
+#' @references 
+#'   ISO 19115:2003 - Geographic information -- Metadata 
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISOThematicClassificationCorrectness <- R6Class("ISOThematicClassificationCorrectness",
+  inherit = ISOAbstractTemporalAccuracy,
+  private = list(
+   xmlElement = "DQ_ThematicClassificationCorrectness",
+   xmlNamespacePrefix = "GMD"
+  ),
+  public = list()
+)
---
+++ tests/testthat/test_ISODataQualityThematicAccuracy.R
@@ -0,0 +1,149 @@
+# test_ISODataQualityTemporalAccuracy.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for ISODataQualityTemporalAccuracy.R
+#=======================
+require(geometa, quietly = TRUE)
+require(testthat)
+
+context("ISODataQualityThematicAccuracy")
+
+test_that("ISOAbstractThematicAccuracy",{
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  #encoding
+  dq <- ISOAbstractThematicAccuracy$new()
+  dq$addNameOfMeasure("measure")
+  metaId <- ISOMetaIdentifier$new(code = "measure-id")
+  dq$setMeasureIdentification(metaId)
+  dq$setMeasureDescription("description")
+  dq$setEvaluationMethodDescription("method description")
+  dq$setEvaluationMethodType("indirect")
+  dq$setDateTime(ISOdate(2015,1,1,12,10,49))
+  spec <- ISOCitation$new()
+  spec$setTitle("specification title")
+  spec$setAlternateTitle("specification alternate title")
+  d <- ISODate$new()
+  d$setDate(ISOdate(2015, 1, 1, 1))
+  d$setDateType("publication")
+  spec$addDate(d)
+  dq$setEvaluationProcedure(spec)
+  result <- ISOConformanceResult$new()
+  result$setSpecification(spec)
+  result$setExplanation("some explanation about the conformance")
+  result$setPass(TRUE)
+  dq$addResult(result)
+  xml <- dq$encode(validate=F)
+  expect_is(xml, "XMLInternalNode")
+  #decoding
+  dq2 <- ISOAbstractThematicAccuracy$new(xml = xml)
+  xml2 <- dq2$encode(validate=F)
+  #identity
+  expect_true(ISOAbstractObject$compare(dq, dq2))
+  
+})
+
+test_that("ISOQuantitativeAttributeAccuracy",{
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  #encoding
+  dq <- ISOQuantitativeAttributeAccuracy$new()
+  dq$addNameOfMeasure("measure")
+  metaId <- ISOMetaIdentifier$new(code = "measure-id")
+  dq$setMeasureIdentification(metaId)
+  dq$setMeasureDescription("description")
+  dq$setEvaluationMethodDescription("method description")
+  dq$setEvaluationMethodType("indirect")
+  dq$setDateTime(ISOdate(2015,1,1,12,10,49))
+  spec <- ISOCitation$new()
+  spec$setTitle("specification title")
+  spec$setAlternateTitle("specification alternate title")
+  d <- ISODate$new()
+  d$setDate(ISOdate(2015, 1, 1, 1))
+  d$setDateType("publication")
+  spec$addDate(d)
+  dq$setEvaluationProcedure(spec)
+  result <- ISOConformanceResult$new()
+  result$setSpecification(spec)
+  result$setExplanation("some explanation about the conformance")
+  result$setPass(TRUE)
+  dq$addResult(result)
+  xml <- dq$encode()
+  expect_is(xml, "XMLInternalNode")
+  #decoding
+  dq2 <- ISOQuantitativeAttributeAccuracy$new(xml = xml)
+  xml2 <- dq2$encode()
+  #identity
+  expect_true(ISOAbstractObject$compare(dq, dq2))
+  
+})
+
+test_that("ISONonQuantitativeAttributeAccuracy",{
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  #encoding
+  dq <- ISONonQuantitativeAttributeAccuracy$new()
+  dq$addNameOfMeasure("measure")
+  metaId <- ISOMetaIdentifier$new(code = "measure-id")
+  dq$setMeasureIdentification(metaId)
+  dq$setMeasureDescription("description")
+  dq$setEvaluationMethodDescription("method description")
+  dq$setEvaluationMethodType("indirect")
+  dq$setDateTime(ISOdate(2015,1,1,12,10,49))
+  spec <- ISOCitation$new()
+  spec$setTitle("specification title")
+  spec$setAlternateTitle("specification alternate title")
+  d <- ISODate$new()
+  d$setDate(ISOdate(2015, 1, 1, 1))
+  d$setDateType("publication")
+  spec$addDate(d)
+  dq$setEvaluationProcedure(spec)
+  result <- ISOConformanceResult$new()
+  result$setSpecification(spec)
+  result$setExplanation("some explanation about the conformance")
+  result$setPass(TRUE)
+  dq$addResult(result)
+  xml <- dq$encode()
+  expect_is(xml, "XMLInternalNode")
+  #decoding
+  dq2 <- ISONonQuantitativeAttributeAccuracy$new(xml = xml)
+  xml2 <- dq2$encode()
+  #identity
+  expect_true(ISOAbstractObject$compare(dq, dq2))
+  
+})
+
+test_that("ISOThematicClassificationCorrectness",{
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  #encoding
+  dq <- ISOThematicClassificationCorrectness$new()
+  dq$addNameOfMeasure("measure")
+  metaId <- ISOMetaIdentifier$new(code = "measure-id")
+  dq$setMeasureIdentification(metaId)
+  dq$setMeasureDescription("description")
+  dq$setEvaluationMethodDescription("method description")
+  dq$setEvaluationMethodType("indirect")
+  dq$setDateTime(ISOdate(2015,1,1,12,10,49))
+  spec <- ISOCitation$new()
+  spec$setTitle("specification title")
+  spec$setAlternateTitle("specification alternate title")
+  d <- ISODate$new()
+  d$setDate(ISOdate(2015, 1, 1, 1))
+  d$setDateType("publication")
+  spec$addDate(d)
+  dq$setEvaluationProcedure(spec)
+  result <- ISOConformanceResult$new()
+  result$setSpecification(spec)
+  result$setExplanation("some explanation about the conformance")
+  result$setPass(TRUE)
+  dq$addResult(result)
+  xml <- dq$encode()
+  expect_is(xml, "XMLInternalNode")
+  #decoding
+  dq2 <- ISOThematicClassificationCorrectness$new(xml = xml)
+  xml2 <- dq2$encode()
+  #identity
+  expect_true(ISOAbstractObject$compare(dq, dq2))
+  
+})
