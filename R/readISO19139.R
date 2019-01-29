b19001ff139ee224a55eae5c6b30fc046817acb9
eblondel <emmanuel.blondel1@gmail.com>
2019-01-30 00:52:07 +0100

    #120 suppress warnings for readLines

--- R/readISO19139.R
+++ R/readISO19139.R
@@ -37,7 +37,7 @@ readISO19139 <- function(file = NULL, url = NULL, raw = FALSE){
     doc <- content(req, as = "text", encoding = encoding)
     raw_xml <- XML::xmlParse(doc, encoding = encoding)
   }else{
-    raw_xml <- readLines(file, encoding = encoding)
+    raw_xml <- suppressWarnings(readLines(file, encoding = encoding))
     raw_xml <- paste0(raw_xml, collapse="") 
     if(Encoding(raw_xml) != "UTF-8") Encoding(raw_xml) <- "UTF-8"
     if(Encoding(raw_xml) == "unknown"){
