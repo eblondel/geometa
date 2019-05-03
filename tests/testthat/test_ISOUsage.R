28521a7a6d35c7b21d7f07bd9de35ad77c177631
eblondel <emmanuel.blondel1@gmail.com>
2019-05-03 16:33:00 +0200

    #134 ISOUsage

---
+++ tests/testthat/test_ISOUsage.R
@@ -0,0 +1,54 @@
+# test_ISOBoundingPolygon.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for ISOBoundingPolygon.R
+#=======================
+require(geometa, quietly = TRUE)
+require(sf)
+require(testthat)
+
+context("ISOBoundingPolygon")
+
+test_that("encoding",{
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  #encoding
+  md <- ISOUsage$new()
+  md$setSpecificUsage("some usage")
+  md$setUsageDateTime(Sys.time())
+  md$setUserDeterminedLimitations("some limitations")
+  for(i in 1:3){
+    rp <- ISOResponsibleParty$new()
+    rp$setIndividualName(paste0("Firstname",i," Lastname",i))
+    rp$setOrganisationName("somewhere")
+    rp$setPositionName(paste0("someposition",i))
+    rp$setRole("pointOfContact")
+    contact <- ISOContact$new()
+    phone <- ISOTelephone$new()
+    phone$setVoice(paste0("myphonenumber",i))
+    phone$setFacsimile(paste0("myfacsimile",i))
+    contact$setPhone(phone)
+    address <- ISOAddress$new()
+    address$setDeliveryPoint("theaddress")
+    address$setCity("thecity")
+    address$setPostalCode("111")
+    address$setCountry("France")
+    address$setEmail("someone@theorg.org")
+    contact$setAddress(address)
+    res <- ISOOnlineResource$new()
+    res$setLinkage("http://somelink")
+    res$setName("someresourcename")
+    contact$setOnlineResource(res)
+    rp$setContactInfo(contact)
+    md$addUserContact(rp)
+  }
+  xml <- md$encode()
+  expect_is(xml, "XMLInternalNode")
+  
+  #decoding
+  md2 <- ISOUsage$new(xml = xml)
+  xml2 <- md2$encode()
+  
+  expect_true(ISOAbstractObject$compare(md, md2))
+  
+})
