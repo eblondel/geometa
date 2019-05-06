8caf74b5c82edcb1891680193a9b4af3f8ae587c
eblondel <emmanuel.blondel1@gmail.com>
2019-05-06 11:54:41 +0200

    #142 DS_Association --> ISOAssociation

---
+++ R/ISOAssociation.R
@@ -0,0 +1,37 @@
+#' ISOAssociation
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO association
+#' @return Object of \code{\link{R6Class}} for modelling an ISOAssociation
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml)}}{
+#'    This method is used to instantiate an ISOAssociation
+#'  }
+#' }
+#' 
+#' @references 
+#'   ISO 19115:2003 - Geographic information -- Metadata 
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISOAssociation <- R6Class("ISOAssociation",
+  inherit = ISOAbstractObject,
+  private = list(
+    xmlElement = "DS_Association",
+    xmlNamespacePrefix = "GMD"
+  ),
+  public = list(
+    specificUsage = NULL,
+    usageDateTime = NULL,
+    userDeterminedLimitations = NULL,
+    userContactInfo = list(),
+    initialize = function(xml = NULL){
+      super$initialize(xml = xml)
+    }
+  )                                          
+)
