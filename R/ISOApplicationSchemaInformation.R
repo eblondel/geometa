e56137e7699b3e32ff6f8d2fe36c008f416c5687
eblondel <emmanuel.blondel1@gmail.com>
2019-05-09 00:11:52 +0200

    #144 MD_ApplicationSchemaInformation --> ISOApplicationSchemaInformation

---
+++ R/ISOApplicationSchemaInformation.R
@@ -0,0 +1,108 @@
+#' ISOApplicationSchemaInformation
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO application schema information
+#' @return Object of \code{\link{R6Class}} for modelling an ISO ApplicationSchemaInformation
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Methods:
+#' \describe{
+#'  \item{\code{new(xml)}}{
+#'    This method is used to instantiate an ISOApplicationSchemaInformation
+#'  }
+#'  \item{\code{setName(name)}}{
+#'    Sets name, object of class \code{ISOCitation}
+#'  }
+#'  \item{\code{setSchemaLanguage(schemaLanguage)}}{
+#'    Sets schema language
+#'  }
+#'  \item{\code{setConstraintLanguage(constraintLanguage)}}{
+#'    Sets constraint language
+#'  }
+#'  \item{\code{setSchemaAscii(schemaAscii)}}{
+#'    Sets schema Ascii
+#'  }
+#'  \item{\code{setGraphicsFile(graphicsFile)}}{
+#'    Sets graphics file
+#'  }
+#'  \item{\code{setSoftwareDevelopmentFile(file)}}{
+#'    Sets software development file
+#'  }
+#'  \item{\code{setSoftwareDevelopmentFileFormat(format)}}{
+#'    Sets software development file format
+#'  }
+#' }
+#' 
+#' @references 
+#'   ISO 19115:2003 - Geographic information -- Metadata 
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISOApplicationSchemaInformation <- R6Class("ISOApplicationSchemaInformation",
+   inherit = ISOAbstractObject,
+   private = list(
+     xmlElement = "MD_ApplicationSchemaInformation",
+     xmlNamespacePrefix = "GMD"
+   ),
+   public = list(
+     
+     #name [1..1]
+     name = NULL,
+     #schemaLanguage [1..1]
+     schemaLanguage = NULL,
+     #constraintLanguage [1..1]  
+     constraintLanguage = NULL,
+     #schemaAscii [0..1]
+     schemaAscii = NULL,
+     #graphicsFile [0..1] 
+     graphicsFile = NULL,
+     #softwareDevelopmentFile [0..1]
+     softwareDevelopmentFile = NULL,
+     #softwareDevelopmentFileFormat [0..1]
+     softwareDevelopmentFileFormat = NULL,
+    
+     initialize = function(xml = NULL){
+       super$initialize(xml = xml)
+     },
+     
+     #setName
+     setName = function(name){
+       if(!is(name, "ISOCitation")){
+         stop("The argument 'name' should be an object of class 'ISOCitation")
+       }
+       self$name <- name
+     },
+     
+     #setSchemaLanguage
+     setSchemaLanguage = function(schemaLanguage){
+       self$schemaLanguage <- schemaLanguage
+     },
+     
+     #setConstraintLanguage
+     setConstraintLanguage = function(constraintLanguage){
+       self$constraintLanguage <- constraintLanguage
+     },
+     
+     #setSchemaAscii
+     setSchemaAscii = function(schemaAscii){
+       self$schemaAscii <- schemaAscii
+     },
+     
+     #setGraphicsFile
+     setGraphicsFile = function(graphicsFile){
+       self$graphicsFile <- ISOBinary$new(value = graphicsFile)
+     },
+     
+     #setSoftwareDevelopmentFile
+     setSoftwareDevelopmentFile = function(file){
+       self$softwareDevelopmentFile <- ISOBinary$new(value = file)
+     },
+     
+     #setSoftwareDevelopmentFileFormat
+     setSoftwareDevelopmentFileFormat = function(format){
+       self$softwareDevelopmentFileFormat <- format
+     }
+   )                        
+)
---
+++ tests/testthat/test_ISOApplicationSchemaInformation.R
@@ -0,0 +1,75 @@
+# test_ISOApplicationSchemaInformation.R
+# Author: Emmanuel Blondel <emmanuel.blondel1@gmail.com>
+#
+# Description: Unit tests for ISOApplicationSchemaInformation.R
+#=======================
+require(geometa, quietly = TRUE)
+require(testthat)
+
+context("ISOApplicationSchemaInformation")
+
+test_that("encoding",{
+  testthat::skip_on_cran()
+  testthat::skip_on_travis()
+  #encoding
+  md <- ISOApplicationSchemaInformation$new()
+  
+  rp <- ISOResponsibleParty$new()
+  rp$setIndividualName("John Who")
+  rp$setOrganisationName("somewhere")
+  rp$setPositionName("someposition")
+  rp$setRole("pointOfContact")
+  contact <- ISOContact$new()
+  phone <- ISOTelephone$new()
+  phone$setVoice("myphonenumber")
+  phone$setFacsimile("myfacsimile")
+  contact$setPhone(phone)
+  address <- ISOAddress$new()
+  address$setDeliveryPoint("theaddress")
+  address$setCity("thecity")
+  address$setPostalCode("111")
+  address$setCountry("France")
+  address$setEmail("someone@theorg.org")
+  contact$setAddress(address)
+  res <- ISOOnlineResource$new()
+  res$setLinkage("http://somelink")
+  res$setName("somename")
+  contact$setOnlineResource(res)
+  rp$setContactInfo(contact)
+  
+  #citation
+  ct <- ISOCitation$new()
+  ct$setTitle("sometitle")
+  d1 <- ISODate$new()
+  d1$setDate(ISOdate(2015, 1, 1, 1))
+  d1$setDateType("creation")
+  ct$addDate(d1)
+  d2 <- ISODate$new()
+  d2$setDate(ISOdate(2015, 3, 31, 1))
+  d2$setDateType("publication")
+  ct$addDate(d2)
+  ct$setEdition("1.0")
+  ct$setEditionDate(as.Date(ISOdate(2015, 1, 1, 1)))
+  ct$setIdentifier(ISOMetaIdentifier$new(code = "identifier"))
+  ct$setPresentationForm("mapDigital")
+  ct$setCitedResponsibleParty(rp)
+  
+  md$setName(ct)
+  
+  md$setSchemaLanguage("string")
+  md$setConstraintLanguage("string")
+  md$setSchemaAscii("string")
+  md$setGraphicsFile("string")
+  md$setSoftwareDevelopmentFile("string")
+  md$setSoftwareDevelopmentFileFormat("string")
+  
+  xml <- md$encode()
+  expect_is(xml, "XMLInternalNode")
+  
+  #decoding
+  md2 <- ISOApplicationSchemaInformation$new(xml = xml)
+  xml2 <- md2$encode()
+  
+  expect_true(ISOAbstractObject$compare(md, md2))
+  
+})
