44671d3e70ba2fbfcd2f29bb058e688391f4b337
eblondel <emmanuel.blondel1@gmail.com>
2019-05-27 13:59:04 +0200

    #150 more codelist classes classes

--- R/ISOImageryPolarisationOrientation.R
+++ R/ISOImageryPolarisationOrientation.R
@@ -1,35 +1,35 @@
-#' ISOImageryPolarizationOrientation
+#' ISOImageryPolarisationOrientation
 #'
 #' @docType class
 #' @importFrom R6 R6Class
 #' @export
-#' @keywords ISO imagery polarization orientation
-#' @return Object of \code{\link{R6Class}} for modelling an ISO Imagery polarization orientation
+#' @keywords ISO imagery Polarisation orientation
+#' @return Object of \code{\link{R6Class}} for modelling an ISO Imagery Polarisation orientation
 #' @format \code{\link{R6Class}} object.
 #'
 #' @section Methods:
 #' \describe{
 #'  \item{\code{new(xml,value)}}{
-#'    This method is used to instantiate an \code{\link{ISOImageryPolarizationOrientation}}
+#'    This method is used to instantiate an \code{\link{ISOImageryPolarisationOrientation}}
 #'  }
 #' }
 #' 
 #' @examples
 #'   #possible values
-#'   values <- ISOImageryPolarizationOrientation$values(labels = TRUE)
+#'   values <- ISOImageryPolarisationOrientation$values(labels = TRUE)
 #'   
 #'   #some def
-#'   h <- ISOImageryPolarizationOrientation$new(value = "horizontal")
+#'   h <- ISOImageryPolarisationOrientation$new(value = "horizontal")
 #' 
 #' @references 
 #'   ISO 19115-2:2009 - Geographic information -- Metadata Part 2: Extensions for imagery and gridded data
 #' 
 #' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
 #'
-ISOImageryPolarizationOrientation <- R6Class("ISOImageryPolarizationOrientation",
+ISOImageryPolarisationOrientation <- R6Class("ISOImageryPolarisationOrientation",
    inherit = ISOCodeListValue,
    private = list(
-     xmlElement = "MI_PolarizationOrientationCode",
+     xmlElement = "MI_PolarisationOrientationCode",
      xmlNamespacePrefix = "GMI"
    ),
    public = list(
@@ -40,6 +40,6 @@ ISOImageryPolarizationOrientation <- R6Class("ISOImageryPolarizationOrientation"
    )                        
 )
 
-ISOImageryPolarizationOrientation$values <- function(labels = FALSE){
-  return(ISOCodeListValue$values(ISOImageryPolarizationOrientation, labels))
+ISOImageryPolarisationOrientation$values <- function(labels = FALSE){
+  return(ISOCodeListValue$values(ISOImageryPolarisationOrientation, labels))
 }
