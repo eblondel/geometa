b9c63a541ef5101668c9b692e266a058cf818d36
eblondel <emmanuel.blondel1@gmail.com>
2019-07-30 13:44:02 +0200

    missing fields in ISOCitation

--- R/ISOCitation.R
+++ R/ISOCitation.R
@@ -8,13 +8,18 @@
 #' @format \code{\link{R6Class}} object.
 #'
 #' @field title [\code{\link{character}}] title
-#' @field alternateTitle [\code{\link{character}}] alternateTitle
+#' @field alternateTitle [list of \code{\link{character}}] alternateTitle
 #' @field date [\code{\link{ISODate}}] the citation date with date type
 #' @field edition [\code{\link{character}}] citation edition
 #' @field editionDate [\code{\link{Date}}|\code{\link{POSIXt}}] date or date/time of edition
-#' @field identifier [\code{\link{ISOMetaIdentifier}}] identifier
-#' @field citedResponsibleParty [\code{\link{ISOResponsibleParty}}] responsible party
-#' @field presentationForm [\code{\link{ISOPresentationForm}}] presentation form
+#' @field identifier [list of \code{\link{ISOMetaIdentifier}}] identifier
+#' @field citedResponsibleParty [list of \code{\link{ISOResponsibleParty}}] responsible party
+#' @field presentationForm [list of \code{\link{ISOPresentationForm}}] presentation form
+#' @field series [\code{link{ISOCitationSeries}}] series
+#' @field otherCitationDetails [\code{\link{character}}] other citation details
+#' @field collectiveTitle [\code{\link{character}}] collective title
+#' @field ISBN [\code{\link{character}}] ISBN
+#' @field ISSN [\code{\link{character}}] ISSN
 #'
 #' @section Methods:
 #' \describe{
@@ -27,6 +32,14 @@
 #'  }
 #'  \item{\code{setAlternateTitle(alternateTitle, locales)}}{
 #'    Sets an alternate title. Locale names can be specified as \code{list}
+#'    with the \code{locales} argument.
+#'  }
+#'  \item{\code{addAlternateTitle(alternateTitle, locales)}}{
+#'    Adds an alternate title. Locale names can be specified as \code{list}
+#'    with the \code{locales} argument.
+#'  }
+#'  \item{\code{delAlternateTitle(alternateTitle, locales)}}{
+#'    Adds an alternate title. Locale names can be specified as \code{list}
 #'    with the \code{locales} argument.
 #'  }
 #'  \item{\code{addDate(date)}}{
@@ -44,11 +57,40 @@
 #'  \item{\code{setIdentifier(identifier)}}{
 #'    Sets the identifier as object of class \code{\link{ISOMetaIdentifier}}
 #'  }
+#'  \item{\code{addIdentifier(identifier)}}{
+#'    Adds an identifier as object of class \code{\link{ISOMetaIdentifier}}
+#'  }
+#'  \item{\code{delIdentifier(identifier)}}{
+#'    Deletes an identifier as object of class \code{\link{ISOMetaIdentifier}}
+#'  }
 #'  \item{\code{seCitedResponsibleParty(rp)}}{
 #'    Sets the cited responsiblep party, object of class \code{\link{ISOResponsibleParty}}
 #'  }
-#'  \item{\code{setPresentationForm}}{
+#'  \item{\code{setPresentationForm(presentationForm)}}{
 #'    Sets the presentation form, object of class \code{\link{ISOPresentationForm}}
+#'  }
+#'  \item{\code{addPresentationForm(presentationForm)}}{
+#'    Adds a presentation form, object of class \code{\link{ISOPresentationForm}}
+#'  }
+#'  \item{\code{delPresentationForm(presentationForm)}}{
+#'    Deletes a presentation form, object of class \code{\link{ISOPresentationForm}}
+#'  }
+#'  \item{\code{setSeries(series)}}{
+#'    Set series, object of class \code{\link{ISOCitationSeries}}
+#'  }
+#'  \item{\code{setOtherCitationDetails(otherCitationDetails, locales)}}{
+#'    Set other citation details. Locale names can be specified as \code{list}
+#'    with the \code{locales} argument.
+#'  }
+#'  \item{\code{setCollectiveTitle(collectiveTitle, locales)}}{
+#'    Set collective title. Locale names can be specified as \code{list}
+#'    with the \code{locales} argument.
+#'  }
+#'  \item{\code{setISBN(isbn)}}{
+#'    Set the ISBN
+#'  }
+#'  \item{\code{setISSN(issn)}}{
+#'    Set the ISSN
 #'  }
 #' }
 #' 
@@ -100,13 +142,18 @@ ISOCitation<- R6Class("ISOCitation",
   ),
   public = list(
     title = NULL,
-    alternateTitle = NULL,
+    alternateTitle = list(),
     date = list(),
     edition = NULL,
     editionDate = NULL,
-    identifier = NULL,
-    citedResponsibleParty = NULL,
-    presentationForm = NULL,
+    identifier = list(),
+    citedResponsibleParty = list(),
+    presentationForm = list(),
+    series = NULL,
+    otherCitationDetails = NULL,
+    collectiveTitle = NULL,
+    ISBN = NULL,
+    ISSN = NULL,
     initialize = function(xml = NULL){
       super$initialize(xml = xml)
     },
@@ -134,6 +181,7 @@ ISOCitation<- R6Class("ISOCitation",
     
     #setAlternateTitle
     setAlternateTitle = function(alternateTitle, locales = NULL){
+      warning("'setAlternateTitle' is deprecated, use 'addAlternateTitle' instead")
       classPass <- TRUE
       if(is.null(alternateTitle)){
         classPath <- FALSE
@@ -151,6 +199,48 @@ ISOCitation<- R6Class("ISOCitation",
       if(!is.null(locales)){
         self$alternateTitle <- self$createLocalisedProperty(alternateTitle, locales)
       }
+    },
+    
+    #addAlternateTitle
+    addAlternateTitle = function(alternateTitle, locales = NULL){
+      classPass <- TRUE
+      if(is.null(alternateTitle)){
+        classPath <- FALSE
+      }else{
+        if(!inherits(alternateTitle,"ISOAbstractObject")){
+          if(!(is.na(alternateTitle) || is(alternateTitle, "character"))) classPass <- FALSE
+        }else{
+          if(is(alternateTitle,"ISOAnchor")){ classPass <- TRUE }else{ classPass <- FALSE }
+        }
+      }
+      if(!classPass){
+        stop("Alternate title should be an object of class 'character' or 'ISOAnchor'")
+      }
+      if(!is.null(locales)){
+        alternateTitle <- self$createLocalisedProperty(alternateTitle, locales)
+      }
+      return(self$addListElement("alternateTitle", alternateTitle))
+    },
+    
+    #delAlternateTitle
+    delAlternateTitle = function(alternateTitle, locales = NULL){
+      classPass <- TRUE
+      if(is.null(alternateTitle)){
+        classPath <- FALSE
+      }else{
+        if(!inherits(alternateTitle,"ISOAbstractObject")){
+          if(!(is.na(alternateTitle) || is(alternateTitle, "character"))) classPass <- FALSE
+        }else{
+          if(is(alternateTitle,"ISOAnchor")){ classPass <- TRUE }else{ classPass <- FALSE }
+        }
+      }
+      if(!classPass){
+        stop("Alternate title should be an object of class 'character' or 'ISOAnchor'")
+      }
+      if(!is.null(locales)){
+        alternateTitle <- self$createLocalisedProperty(alternateTitle, locales)
+      }
+      return(self$delListElement("alternateTitle", alternateTitle))
     },
     
     #addDate
@@ -176,26 +266,111 @@ ISOCitation<- R6Class("ISOCitation",
     
     #setIdentifier
     setIdentifier = function(identifier){
+      warning("'setIdentifier' method is deprecated, use 'addIdentifier' instead")
       if(!is(identifier, "ISOMetaIdentifier")){
         stop("The argument should be a 'ISOMetaIdentifier' object")
       }
       self$identifier <- identifier
     },
     
+    #addIdentifier
+    addIdentifier = function(identifier){
+      if(!is(identifier, "ISOMetaIdentifier")){
+        stop("The argument should be a 'ISOMetaIdentifier' object")
+      }
+      return(self$addListElement("identifier", identifier))
+    },
+
+    #delIdentifier
+    delIdentifier = function(identifier){
+      if(!is(identifier, "ISOMetaIdentifier")){
+        stop("The argument should be a 'ISOMetaIdentifier' object")
+      }
+      return(self$delListElement("identifier", identifier))
+    },
+    
     #setCitedResponsibleParty
     setCitedResponsibleParty = function(rp){
+      warning("'setCitedResponsibleParty' method is deprecated, use 'addCitedResponsibleParty' instead")
       if(!is(rp, "ISOResponsibleParty")){
         stop("The argument should be a 'ISOResponsibleParty' object")
       }
       self$citedResponsibleParty <- rp
     },
     
+    #addCitedResponsibleParty
+    addCitedResponsibleParty = function(rp){
+      if(!is(rp, "ISOResponsibleParty")){
+        stop("The argument should be a 'ISOResponsibleParty' object")
+      }
+      return(self$addListElement("citedResponsibleParty", rp))
+    },
+    
+    #delCitedResponsibleParty
+    delCitedResponsibleParty = function(rp){
+      if(!is(rp, "ISOResponsibleParty")){
+        stop("The argument should be a 'ISOResponsibleParty' object")
+      }
+      return(self$delListElement("citedResponsibleParty", rp))
+    },
+    
     #setPresentationForm
     setPresentationForm = function(presentationForm){
+      warning("'setPresentationForm' method is deprecated, use 'addPresentationForm' instead")
       if(is(presentationForm, "character")){
         presentationForm <- ISOPresentationForm$new(value = presentationForm)
       }
       self$presentationForm <- presentationForm
+    },
+    
+    #addPresentationForm
+    addPresentationForm = function(presentationForm){
+      if(is(presentationForm, "character")){
+        presentationForm <- ISOPresentationForm$new(value = presentationForm)
+      }
+      self$addListElement("presentationForm", presentationForm)
+    },
+    
+    #delPresentationForm
+    delPresentationForm = function(presentationForm){
+      if(is(presentationForm, "character")){
+        presentationForm <- ISOPresentationForm$new(value = presentationForm)
+      }
+      self$delListElement("presentationForm", presentationForm)
+    },
+    
+    #setSeries
+    setSeries = function(series){
+      if(!is(series, "ISOCitationSeries")){
+        stop("The argument should be a 'ISOCitationSeries' object")
+      }
+      self$series <- series
+    },
+    
+    #setOtherCitationDetails
+    setOtherCitationDetails = function(otherCitationDetails, locales = NULL){
+      self$otherCitationDetails <- otherCitationDetails
+      if(!is.null(locales)){
+        self$otherCitationDetails <- self$createLocalisedProperty(otherCitationDetails, locales)
+      }
+    },
+    
+    #setCollectiveTitle
+    setCollectiveTitle = function(collectiveTitle, locales = NULL){
+      self$collectiveTitle <- collectiveTitle
+      if(!is.null(locales)){
+        self$collectiveTitle <- self$createLocalisedProperty(collectiveTitle, locales)
+      }
+    },
+    
+    #setISBN
+    setISBN = function(isbn){
+      self$ISBN <- isbn
+    },
+    
+    #setISSN
+    setISSN = function(issn){
+      self$ISSN <- issn
     }
     
   )                                          
