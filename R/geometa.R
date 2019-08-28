30505f7e49e56c585247ba55bcc6bc11972d5f8a
eblondel <emmanuel.blondel1@gmail.com>
2019-08-28 10:07:14 +0200

    update pkg date

--- R/geometa.R
+++ R/geometa.R
@@ -12,7 +12,7 @@
 #'    Package: \tab geometa\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 0.6-0\cr
-#'    Date: \tab 2019-08-19\cr
+#'    Date: \tab 2019-08-28\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
