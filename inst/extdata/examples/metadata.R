7ffbe702e9309e5ae3cd58ea35142a8baa3949b0
eblondel <emmanuel.blondel1@gmail.com>
2019-09-03 17:42:15 +0200

    #162 provide INSPIRE valid metadata

---
+++ inst/extdata/examples/metadata.R
@@ -0,0 +1,261 @@
+#Example of ISO 19115/19139 metadata generated with geometa
+#This example is valid according to INSPIRE common metadata requirements
+
+md = ISOMetadata$new()
+md$setFileIdentifier("my-metadata-identifier")
+md$setParentIdentifier("my-parent-metadata-identifier")
+md$setCharacterSet("utf8")
+md$setLanguage("eng")
+md$setDateStamp(ISOdate(2015, 1, 1, 1))
+md$setMetadataStandardName("ISO 19115:2003/19139")
+md$setMetadataStandardVersion("1.0")
+md$setDataSetURI("my-dataset-identifier")
+
+#add 3 contacts
+for(i in 1:3){
+  rp <- ISOResponsibleParty$new()
+  rp$setIndividualName(paste0("someone",i))
+  rp$setOrganisationName("somewhere")
+  rp$setPositionName(paste0("someposition",i))
+  rp$setRole("pointOfContact")
+  contact <- ISOContact$new()
+  phone <- ISOTelephone$new()
+  phone$setVoice(paste0("myphonenumber",i))
+  phone$setFacsimile(paste0("myfacsimile",i))
+  contact$setPhone(phone)
+  address <- ISOAddress$new()
+  address$setDeliveryPoint("theaddress")
+  address$setCity("thecity")
+  address$setPostalCode("111")
+  address$setCountry("France")
+  address$setEmail("someone@theorg.org")
+  contact$setAddress(address)
+  res <- ISOOnlineResource$new()
+  res$setLinkage("http://somelink")
+  res$setName("someresourcename")
+  contact$setOnlineResource(res)
+  rp$setContactInfo(contact)
+  md$addContact(rp)
+}
+
+#VectorSpatialRepresentation
+vsr <- ISOVectorSpatialRepresentation$new()
+vsr$setTopologyLevel("geometryOnly")
+geomObject <- ISOGeometricObjects$new()
+geomObject$setGeometricObjectType("surface")
+geomObject$setGeometricObjectCount(5L)
+vsr$setGeometricObjects(geomObject)
+md$addSpatialRepresentationInfo(vsr)
+
+#ReferenceSystem
+rs <- ISOReferenceSystem$new()
+rsId <- ISOReferenceIdentifier$new(code = "4326", codeSpace = "EPSG")
+rs$setReferenceSystemIdentifier(rsId)
+md$setReferenceSystemInfo(rs)
+
+#data identification
+ident <- ISODataIdentification$new()
+ident$setAbstract("abstract")
+ident$setPurpose("purpose")
+ident$addCredit("credit1")
+ident$addCredit("credit2")
+ident$addCredit("credit3")
+ident$addStatus("completed")
+ident$setLanguage("eng")
+ident$setCharacterSet("utf8")
+ident$addTopicCategory("biota")
+ident$addTopicCategory("oceans")
+
+#adding a point of contact
+rp <- ISOResponsibleParty$new()
+rp$setIndividualName("someone")
+rp$setOrganisationName("somewhere")
+rp$setPositionName("someposition")
+rp$setRole("pointOfContact")
+contact <- ISOContact$new()
+phone <- ISOTelephone$new()
+phone$setVoice("myphonenumber")
+phone$setFacsimile("myfacsimile")
+contact$setPhone(phone)
+address <- ISOAddress$new()
+address$setDeliveryPoint("theaddress")
+address$setCity("thecity")
+address$setPostalCode("111")
+address$setCountry("France")
+address$setEmail("someone@theorg.org")
+contact$setAddress(address)
+res <- ISOOnlineResource$new()
+res$setLinkage("http://somelink")
+res$setName("somename")
+contact$setOnlineResource(res)
+rp$setContactInfo(contact)
+ident$addPointOfContact(rp)
+
+#citation
+ct <- ISOCitation$new()
+ct$setTitle("sometitle")
+d <- ISODate$new()
+d$setDate(ISOdate(2015, 1, 1, 1))
+d$setDateType("publication")
+ct$addDate(d)
+ct$setEdition("1.0")
+ct$setEditionDate(as.Date(ISOdate(2015, 1, 1, 1)))
+ct$addIdentifier(ISOMetaIdentifier$new(code = "identifier"))
+ct$addPresentationForm("mapDigital")
+ct$addCitedResponsibleParty(rp)
+ident$setCitation(ct)
+
+#graphic overview
+go1 <- ISOBrowseGraphic$new(
+  fileName = "http://wwww.somefile.org/png1",
+  fileDescription = "Map Overview 1",
+  fileType = "image/png"
+)
+go2 <- ISOBrowseGraphic$new(
+  fileName = "http://www.somefile.org/png2",
+  fileDescription = "Map Overview 2",
+  fileType = "image/png"
+)
+ident$addGraphicOverview(go1)
+ident$addGraphicOverview(go2)
+
+#maintenance information
+mi <- ISOMaintenanceInformation$new()
+mi$setMaintenanceFrequency("daily")
+ident$setResourceMaintenance(mi)
+
+#adding access legal constraints
+#for INSPIRE controlled terms on access legal constraints, please browse the INSPIRE registry:
+# http://inspire.ec.europa.eu/metadata-codelist/LimitationsOnPublicAccess/
+lc <- ISOLegalConstraints$new()
+lc$addAccessConstraint("otherRestrictions")
+lc$addOtherConstraint(ISOAnchor$new(
+  href = "http://inspire.ec.europa.eu/metadata-codelist/LimitationsOnPublicAccess/INSPIRE_Directive_Article13_1a",
+  name = "public access limited according to Article 13(1)(a) of the INSPIRE Directive"
+))
+ident$addResourceConstraints(lc)
+
+#adding use legal constraints
+#for INSPIRE controlled terms on use legal constraints, please browse the INSPIRE registry:
+# http://inspire.ec.europa.eu/metadata-codelist/ConditionsApplyingToAccessAndUse
+lc2 <- ISOLegalConstraints$new()
+lc2$addUseLimitation("limitation1")
+lc2$addUseLimitation("limitation2")
+lc2$addUseLimitation("limitation3")
+lc2$addAccessConstraint("otherRestrictions")
+lc2$addOtherConstraint(ISOAnchor$new(
+  href = "http://inspire.ec.europa.eu/metadata-codelist/ConditionsApplyingToAccessAndUse/noConditionsApply", 
+  name = "No conditions apply to access and use."
+))
+ident$addResourceConstraints(lc2)
+
+#adding security constraints
+sc <- ISOSecurityConstraints$new()
+sc$setClassification("secret")
+sc$setUserNote("ultra secret")
+sc$setClassificationSystem("no classification in particular")
+sc$setHandlingDescription("description")
+ident$addResourceConstraints(sc)
+
+#adding extent
+extent <- ISOExtent$new()
+bbox <- ISOGeographicBoundingBox$new(minx = -180, miny = -90, maxx = 180, maxy = 90)
+extent$setGeographicElement(bbox)
+ident$setExtent(extent)
+
+#add keywords
+kwds <- ISOKeywords$new()
+kwds$addKeyword("keyword1")
+kwds$addKeyword("keyword2")
+kwds$setKeywordType("theme")
+th <- ISOCitation$new()
+th$setTitle("General")
+th$addDate(d)
+kwds$setThesaurusName(th)
+ident$addKeywords(kwds)
+
+#supplementalInformation
+ident$setSupplementalInformation("some additional information")
+
+#spatial representation type
+ident$addSpatialRepresentationType("vector")
+
+md$setIdentificationInfo(ident)
+
+#Distribution
+distrib <- ISODistribution$new()
+dto <- ISODigitalTransferOptions$new()
+for(i in 1:3){
+  or <- ISOOnlineResource$new()
+  or$setLinkage(paste0("http://somelink",i))
+  or$setName(paste0("name",i))
+  or$setDescription(paste0("description",i))
+  or$setProtocol("WWW:LINK-1.0-http--link")
+  dto$addOnlineResource(or)
+}
+distrib$setDigitalTransferOptions(dto)
+md$setDistributionInfo(distrib)
+
+#create dataQuality object with a 'dataset' scope
+dq <- ISODataQuality$new()
+scope <- ISOScope$new()
+scope$setLevel("dataset")
+dq$setScope(scope)
+
+#add data quality reports...
+
+#add a report the data quality
+dc <- ISODomainConsistency$new()
+result <- ISOConformanceResult$new()
+spec <- ISOCitation$new()
+spec$setTitle("Data Quality check")
+spec$addAlternateTitle("This is is some data quality check report")
+d <- ISODate$new()
+d$setDate(as.Date(ISOdate(2015, 1, 1, 1)))
+d$setDateType("publication")
+spec$addDate(d)
+result$setSpecification(spec)
+result$setExplanation("some explanation about the conformance")
+result$setPass(TRUE)
+dc$addResult(result)
+dq$addReport(dc)
+
+#add INSPIRE reports?
+#INSPIRE - interoperability of spatial data sets and services
+dc_inspire1 <- ISODomainConsistency$new()
+cr_inspire1 <- ISOConformanceResult$new()
+cr_inspire_spec1 <- ISOCitation$new()
+cr_inspire_spec1$setTitle("Commission Regulation (EU) No 1089/2010 of 23 November 2010 implementing Directive 2007/2/EC of the European Parliament and of the Council as regards interoperability of spatial data sets and services")
+cr_inspire1$setExplanation("See the referenced specification")
+cr_inspire_date1 <- ISODate$new()
+cr_inspire_date1$setDate(as.Date(ISOdate(2010,12,8)))
+cr_inspire_date1$setDateType("publication")
+cr_inspire_spec1$addDate(cr_inspire_date1)
+cr_inspire1$setSpecification(cr_inspire_spec1)
+cr_inspire1$setPass(TRUE)
+dc_inspire1$addResult(cr_inspire1)
+dq$addReport(dc_inspire1)
+#INSPIRE - metadata
+dc_inspire2 <- ISODomainConsistency$new()
+cr_inspire2 <- ISOConformanceResult$new()
+cr_inspire_spec2 <- ISOCitation$new()
+cr_inspire_spec2$setTitle("COMMISSION REGULATION (EC) No 1205/2008 of 3 December 2008 implementing Directive 2007/2/EC of the European Parliament and of the Council as regards metadata")
+cr_inspire2$setExplanation("See the referenced specification")
+cr_inspire_date2 <- ISODate$new()
+cr_inspire_date2$setDate(as.Date(ISOdate(2008,12,4)))
+cr_inspire_date2$setDateType("publication")
+cr_inspire_spec2$addDate(cr_inspire_date2)
+cr_inspire2$setSpecification(cr_inspire_spec2)
+cr_inspire2$setPass(TRUE)
+dc_inspire2$addResult(cr_inspire2)
+dq$addReport(dc_inspire2)
+
+#add lineage (more example of lineages in ISOLineage documentation)
+lineage <- ISOLineage$new()
+lineage$setStatement("statement")
+dq$setLineage(lineage)
+
+md$setDataQualityInfo(dq)
+
+#XML representation of the ISOMetadata
+xml <- md$encode()
