3a724e9a14f6298f9f0ddd3b71fb73049cd73f3a
eblondel <emmanuel.blondel1@gmail.com>
2020-02-11 12:23:37 +0100

    #165 fix decimalplaces util function

--- R/ISOBaseDecimal.R
+++ R/ISOBaseDecimal.R
@@ -31,8 +31,8 @@ ISOBaseDecimal <- R6Class("ISOBaseDecimal",
      
      #decimal places
      decimalplaces = function(x) {
-        if ((x %% 1) != 0) {
-           nchar(strsplit(sub('0+$', '', as.character(x)), ".", fixed=TRUE)[[1]][[2]])
+        if (abs(x - round(x)) > .Machine$double.eps^0.5) {
+          nchar(strsplit(sub('0+$', '', as.character(x)), ".", fixed = TRUE)[[1]][[2]])
         } else {
            return(0)
         }
