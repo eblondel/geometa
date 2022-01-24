b9332c40aa8c3b32be7b147dfd6996393ecf6f09
eblondel <emmanuel.blondel1@gmail.com>
2022-01-24 19:59:16 +0100

    update pkg version

--- R/geometa.R
+++ R/geometa.R
@@ -11,7 +11,7 @@
 #'  \tabular{ll}{
 #'    Package: \tab geometa\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 0.6-9999\cr
+#'    Version: \tab 0.7\cr
 #'    Date: \tab 2022-01-24\cr
 #'    License: \tab MIT\cr
 #'    LazyLoad: \tab yes\cr
