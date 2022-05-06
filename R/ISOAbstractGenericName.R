9dbd543fb3afa1fcdf08f86c4324d9fb9b1da9b3
eblondel <emmanuel.blondel1@gmail.com>
2022-05-06 14:24:34 +0200

    fix ISOAbstractGenericName

--- R/ISOAbstractGenericName.R
+++ R/ISOAbstractGenericName.R
@@ -30,7 +30,7 @@ ISOAbstractGenericName <- R6Class("ISOAbstractGenericName",
   public = list(
     value = NA,
     initialize = function(xml = NULL, value = NULL, codeSpace = NULL){
-      super$initialize(xml = NULL, value = NULL, codeSpace = NULL)
+      super$initialize(xml = NULL, value = value, codeSpace = codeSpace)
     }
   )                        
 )
