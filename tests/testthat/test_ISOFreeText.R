99ee44e16a07214b71805fbbd79680137e22940d
eblondel <emmanuel.blondel1@gmail.com>
2022-12-20 20:50:13 +0100

    #203 poc

--- tests/testthat/test_ISOFreeText.R
+++ tests/testthat/test_ISOFreeText.R
@@ -28,3 +28,26 @@ test_that("encoding",{
   expect_true(ISOAbstractObject$compare(md, md2))
   
 })
+
+test_that("encoding - adding xlink:href attributes",{
+  testthat::skip_on_cran()
+  #encoding
+  md <- ISOFreeText$new()
+  str <- ISOLocalisedCharacterString$new(locale = "EN", value = "description")
+  str$parentAttrs = list("xlink:href" = "http://anchor/desc")
+  md$addTextGroup(str)
+  md$addTextGroup(ISOLocalisedCharacterString$new(locale = "FR", value = "description"))
+  md$addTextGroup(ISOLocalisedCharacterString$new(locale = "ES", value = "descripción"))
+  md$addTextGroup(ISOLocalisedCharacterString$new(locale = "AR", value = "تفصيل"))
+  md$addTextGroup(ISOLocalisedCharacterString$new(locale = "RU", value = "резюме"))
+  md$addTextGroup(ISOLocalisedCharacterString$new(locale = "ZN", value = "摘要"))
+  xml <- md$encode()
+  expect_is(xml, "XMLInternalNode")
+  
+  #decoding
+  md2 <- ISOFreeText$new(xml = xml)
+  xml2 <- md2$encode()
+  
+  expect_true(ISOAbstractObject$compare(md, md2))
+  
+})
