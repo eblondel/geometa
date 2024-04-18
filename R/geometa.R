502e3ccba8b7cf3497b3fad4c155ce1debd68eec
eblondel <emmanuel.blondel1@gmail.com>
2024-04-18 09:30:35 +0200

    update rdoc

--- R/geometa.R
+++ R/geometa.R
@@ -1,13 +1,18 @@
 #' @name geometa
 #' @aliases geometa-package
 #' @aliases geometa
-#' @docType package
+"_PACKAGE"
 #' 
 #' @title Tools for Reading and Writing ISO/OGC Geographic Metadata
-#' @description Provides facilities to handle reading and writing of geographic metadata 
-#' defined with OGC/ISO 19115 and 19110 geographic information metadata standards, and 
-#' encoded using the ISO 19139 (XML) standard.
+#' 
+#' @description Provides facilities to read, write and validate geographic metadata 
+#' defined with ISO TC211 / OGC ISO geographic information metadata standards, and 
+#' encoded using the ISO 19139 (XML) standard technical specification. This includes 
+#' the following standards: ISO 19110 (Feature cataloguing), 19115 (dataset metadata),
+#' 19119 (service metadata) and 19136 (GML).
+#' Other interoperable schemas from the OGC are progressively supported as well, such as 
+#' the Sensor Web Enablement (SWE) Common Data Model, the OGC GML Coverage Implementation 
+#' Schema (GMLCOV), or the OGC GML Referenceable Grid (GMLRGRID).
 #'  
 #' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
 #'
-NULL
