f7b535bc3c166d36f4f52a2feda839df0c673929
eblondel <emmanuel.blondel1@gmail.com>
2024-10-04 13:18:45 +0200

    #181 support M5 - DQC classes

---
+++ R/ISOAbstractDataQuality.R
@@ -0,0 +1,31 @@
+#' ISOAbstractDataQuality
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO abstract data quality
+#' @return Object of \code{\link{R6Class}} for modelling an ISO abstract data quality
+#' @format \code{\link{R6Class}} object.
+#'   
+#' @references 
+#'   - ISO 19115-3 \url{https://schemas.isotc211.org/19157/-/dqc/1.2/dqc/#element_Abstract_DataQuality}
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISOAbstractDataQuality <- R6Class("ISOAbstractDataQuality",
+   inherit = ISOAbstractObject,
+   private = list(
+     xmlElement = "Abstract_DataQuality",
+     xmlNamespacePrefix = list(
+       "19115-3" = "DQC"
+     )
+   ),
+   public = list(
+     
+     #'@description Initializes object
+     #'@param xml object of class \link{XMLInternalNode-class}
+     initialize = function(xml = NULL){
+       super$initialize(xml = xml)
+     }
+   )                        
+)
---
+++ R/ISOAbstractQualityElement.R
@@ -0,0 +1,43 @@
+#' ISOAbstractQualityElement
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords ISO abstract quality element
+#' @return Object of \code{\link{R6Class}} for modelling an ISO abstract quality element
+#' @format \code{\link{R6Class}} object.
+#'   
+#' @references 
+#'   - ISO 19115-3 \url{https://schemas.isotc211.org/19157/-/dqc/1.2/dqc/#element_Abstract_QualityElement}
+#' 
+#' @author Emmanuel Blondel <emmanuel.blondel1@@gmail.com>
+#'
+ISOAbstractQualityElement <- R6Class("ISOAbstractQualityElement",
+  inherit = ISOAbstractObject,
+  private = list(
+    xmlElement = "Abstract_QualityElement",
+    xmlNamespacePrefix = list(
+      "19115-3" = "DQC"
+    )
+  ),
+  public = list(
+    
+    #'@field dateTime dateTime
+    dateTime = NULL,
+    
+    #'@description Initializes object
+    #'@param xml object of class \link{XMLInternalNode-class}
+    initialize = function(xml = NULL){
+      super$initialize(xml = xml)
+    },
+    
+    #'@description Set date time
+    #'@param dateTime dateTime object of class \link{ISOBaseDateTime}
+    setDateTime = function(dateTime){
+      if(!is(dateTime, "ISOBaseDateTime")){
+        stop("The argument 'dateTime' should be an object of class 'ISOBaseDateTime'")
+      }
+      self$dateTime = dateTime
+    }
+  )                        
+)
