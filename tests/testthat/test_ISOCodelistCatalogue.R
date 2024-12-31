28509967f1c4b924233c717130f39c8c6d0e4a22
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2024-12-31 15:59:45 +0100

    Update test_ISOCodelistCatalogue.R

--- tests/testthat/test_ISOCodelistCatalogue.R
+++ tests/testthat/test_ISOCodelistCatalogue.R
@@ -11,8 +11,7 @@ context("ISOCodelistCatalogue")
 
 test_that("encoding",{
   testthat::skip_on_cran()
-  #TODO performance!!!!!!
-  system.time(cat <- ISOCodelistCatalogue$new(xml = XML::xmlRoot(XML::xmlParse("inst/extdata/codelists/gmxCodelists.xml"))))
-  system.time(mlcat <- ISOCodelistCatalogue$new(xml = XML::xmlParse("inst/extdata/codelists/ML_gmxCodelists.xml")))
-  system.time(newcat <- ISOCodelistCatalogue$new(xml = XML::xmlParse("inst/extdata/schemas/19115/resources/Codelists/cat/codelists.xml")))
+  #system.time(cat <- ISOCodelistCatalogue$new(xml = XML::xmlRoot(XML::xmlParse("inst/extdata/codelists/gmxCodelists.xml"))))
+  #system.time(mlcat <- ISOCodelistCatalogue$new(xml = XML::xmlParse("inst/extdata/codelists/ML_gmxCodelists.xml")))
+  #system.time(newcat <- ISOCodelistCatalogue$new(xml = XML::xmlParse("inst/extdata/schemas/19115/resources/Codelists/cat/codelists.xml")))
 })
