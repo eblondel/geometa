f0fdd3dce89c1cec1db632b73afcdf73eb393cc0
eblondel <emmanuel.blondel1@gmail.com>
2025-01-02 18:45:06 +0100

    #181 readISO19139 -> readISO

--- R/readISO.R
+++ R/readISO.R
@@ -1,11 +1,11 @@
-#' @name readISO19139
-#' @aliases readISO19139
-#' @title readISO19139
+#' @name readISO
+#' @aliases readISO
+#' @title readISO
 #' @export
-#' @description \code{readISO19139} is a function to read a ISO 19139 from a file
+#' @description \code{readISO} is a function to read a ISO metadata from a file
 #' or url into an object in the \pkg{geometa} model.
 #'
-#' @usage readISO19139(file, url, raw)
+#' @usage readISO(file, url, raw)
 #'                 
 #' @param file a valid file path, as object of class \code{character}
 #' @param url a valid URL, as object of class \code{character}
@@ -18,12 +18,12 @@
 #' @examples
 #' \donttest{
 #'   mdfile <- system.file("extdata/examples", "metadata.xml", package = "geometa")
-#'   md <- readISO19139(mdfile)
+#'   md <- readISO(mdfile)
 #' }
 #' 
 #' @author Emmanuel Blondel, \email{emmanuel.blondel1@@gmail.com}
 #'    
-readISO19139 <- function(file = NULL, url = NULL, raw = FALSE){
+readISO <- function(file = NULL, url = NULL, raw = FALSE){
   
   cacheISOClasses()
   
@@ -57,6 +57,16 @@ readISO19139 <- function(file = NULL, url = NULL, raw = FALSE){
     if(raw){
       out <- raw_xml
     }else{
+      #inspect namespace
+      version <- switch(XML::xmlNamespace(raw_xml),
+                        "http://www.isotc211.org/2005/gmd" = "19139",
+                        "http://standards.iso.org/iso/19115/-3/mdb/2.0" = "19115-3",
+                        "http://www.isotc211.org/2005/gfc" = "19139",
+                        "http://standards.iso.org/iso/19110/gfc/1.1" = "19115-3",
+                        "19139"
+      )
+      setMetadataStandard(version)
+      #instantiate object
       iso_class <- ISOAbstractObject$getISOClassByNode(raw_xml)
       if(!is.null(iso_class)){
         out <- iso_class$new(xml = raw_xml)
@@ -64,5 +74,36 @@ readISO19139 <- function(file = NULL, url = NULL, raw = FALSE){
     }
   }
   return(out)
+}
+
+
+#' @name readISO19139
+#' @aliases readISO19139
+#' @title readISO19139
+#' @export
+#' @description \code{readISO19139} is a function to read a ISO 19139 from a file
+#' or url into an object in the \pkg{geometa} model.
+#'
+#' @usage readISO19139(file, url, raw)
+#'                 
+#' @param file a valid file path, as object of class \code{character}
+#' @param url a valid URL, as object of class \code{character}
+#' @param raw indicates if the function should return the raw XML. By
+#' default this is set to \code{FALSE} and the function will try to map
+#' the xml data to the \pkg{geometa} data model.
+#' 
+#' @return a \pkg{geometa} object inheriting \code{ISOAbstractObject}
+#' 
+#' @examples
+#' \donttest{
+#'   mdfile <- system.file("extdata/examples", "metadata.xml", package = "geometa")
+#'   md <- readISO19139(mdfile)
+#' }
+#' 
+#' @author Emmanuel Blondel, \email{emmanuel.blondel1@@gmail.com}
+#'    
+readISO19139 <- function(file = NULL, url = NULL, raw = FALSE){
+  warnings("'readISO19139' is deprecated, consider using 'readISO' function")
+  readISO(file = file, url = url, raw = raw)
 }
 
