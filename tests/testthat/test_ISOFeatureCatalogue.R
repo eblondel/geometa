c75a886b5f4e74366ea45895c2330c30e38609e3
eblondel <emmanuel.blondel1@gmail.com>
2025-01-23 00:05:26 +0100

    reactivate test_ISOFeatureCatalogue

--- tests/testthat/test_ISOFeatureCatalogue.R
+++ tests/testthat/test_ISOFeatureCatalogue.R
@@ -401,9 +401,7 @@ test_that("encoding - ISO 19115-3",{
   fc2 <- ISOFeatureCatalogue$new(xml = xml)
   xml2 <- fc2$encode()
   
-  if(FALSE){#TO WORK ON
-    expect_true(ISOAbstractObject$compare(fc, fc2, "xml"))
-  }
+  expect_true(ISOAbstractObject$compare(fc, fc2, "xml"))
   
   setMetadataStandard("19139")
   
